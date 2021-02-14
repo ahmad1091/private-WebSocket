4c3a6cac309f6fd3602128c49917e90afa42602f
ahmad1091 <ahmadtayeb91@gmail.com>
2021-02-14 12:09:16 +0200

    initial response handel

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const axios = require("axios");
-const app = express();
-const WebSocket = require("ws");
-const crypto = require("crypto");
-const dotenv = require("dotenv");
-dotenv.config();
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const WebSocket = require('ws')
+const crypto = require('crypto')
+const dotenv = require('dotenv')
+dotenv.config()
 
 // First method: Apply for authentication when establishing a connection.
-const api_key = process.env.api_key;
-const secret = process.env.secret;
+const api_key = process.env.api_key
+const secret = process.env.secret
 // const timestamp = Date.now();
 // const symbol = "BTCUSDT";
 // const query = `api_key=${api_key}&symbol=${symbol}&timestamp=${timestamp}`;
@@ -22,51 +22,87 @@ const secret = process.env.secret;
 //     console.error(err);
 //   });
 
-const wsUrl = "wss://stream-testnet.bybit.com/realtime_private";
+const wsUrl = 'wss://stream-testnet.bybit.com/realtime_private'
 // A UNIX timestamp after which the request become invalid. This is to prevent replay attacks.
 // unit:millisecond
-const expires = Date.now() + 10000;
+const expires = Date.now() + 10000
 // Signature
 const signature = crypto
-  .createHmac("sha256", secret)
-  .update("GET/realtime" + expires)
-  .digest("hex");
+    .createHmac('sha256', secret)
+    .update('GET/realtime' + expires)
+    .digest('hex')
 
 // Parameters string
-const param = `api_key=${api_key}&expires=${expires}&signature=${signature}`;
+const param = `api_key=${api_key}&expires=${expires}&signature=${signature}`
 
 // Establishing connection
-const ws = new WebSocket(wsUrl + "?" + param);
-ws.on("open", () => {
-  console.log("opend " + new Date());
-  ws.send('{"op": "subscribe", "args": ["stop_order"]}');
-  ws.send('{"op": "subscribe", "args": ["execution"]}');
-  ws.send('{"op": "subscribe", "args": ["order"]}');
-  ws.send('{"op": "subscribe", "args": ["position"]}');
-  setInterval(() => {
-    console.log("heart beat ..");
-    ws.send('{"op":"ping"}');
-  }, 35000);
-});
-ws.on("message", (msg) => {
-  console.log("----------------^^^^^^^^^^^^^^^^^^^^^^-------------------", msg);
-});
+ const ws = new WebSocket(wsUrl + '?' + param)
+ws.on('open', () => {
+    console.log('opend ' + new Date())
+     ws.send('{"op": "subscribe", "args": ["stop_order"]}')
+    ws.send('{"op": "subscribe", "args": ["execution"]}')
+    ws.send('{"op": "subscribe", "args": ["order"]}')
+    ws.send('{"op": "subscribe", "args": ["position"]}')
+    setInterval(() => {
+        console.log('heart beat ..')
+        ws.send('{"op":"ping"}')
+    }, 35000)
+})
+ws.on('message', (msg) => {
+    const { topic, action, data, success, ret_msg } = JSON.parse(msg)
+    if (success) {
+        console.log('return msg:', ret_msg)
+    }
+    if (topic === 'order') {
+        console.log(
+            '--------------------topic: ',
+            topic,
+            '---------------------action: ',
+            action
+        )
+        console.log('--------------------- Data: ', data)
+    } else if (topic === 'position') {
+        console.log(
+            '--------------------topic: ',
+            topic,
+            '---------------------action: ',
+            action
+        )
+        console.log('--------------------- Data: ', data)
+    } else if (topic === 'execution') {
+        console.log(
+            '--------------------topic: ',
+            topic,
+            '---------------------action: ',
+            action
+        )
+        console.log('--------------------- Data: ', data)
+    } else if (topic === 'stop_order') {
+        console.log(
+            '--------------------topic: ',
+            topic,
+            '---------------------action: ',
+            action
+        )
+        console.log('--------------------- Data: ', data)
+    }
+})
 
-ws.on("update", function (message) {
-  console.log("update^^^^^^^^^^^^^^", message);
-});
+// ws.on("update", function (message) {
+//   console.log("update^^^^^^^^^^^^^^", message);
+// });
 
-ws.on("response", function (response) {
-  console.log("response", response);
-});
+// ws.on("response", function (response) {
+//   console.log("response", response);
+// });
 
-ws.on("close", function () {
-  console.log("connection closed " + new Date());
-});
+ws.on('close', function () {
+    console.log('connection closed ' + new Date())
+})
 
-ws.on("error", function (err) {
-  console.error("ERR", err);
-});
+ws.on('error', function (err) {
+    console.error('ERR', err)
+})
 // // Second method: Apply for authentication after establishing a connection through auth request.
 
 // const ws = new WebSocket(wsUrl);
@@ -81,8 +117,8 @@ ws.on("error", function (err) {
 //   console.log(msg);
 // });
 
-ws.on("error", (err) => {
-  console.error(err);
-});
+ws.on('error', (err) => {
+    console.error(err)
+})
 
-module.exports = app;
+module.exports = app
